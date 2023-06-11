0ec9fd271a1cfbed6adeea46d3b6bfd5701a6e24
EastDawning <135975417+EastDawning@users.noreply.github.com>
2023-06-11 20:27:29 +0800

    Update chatgpt_llm.py

--- chatgpt_llm.py
+++ chatgpt_llm.py
@@ -29,25 +29,15 @@ class ChatGPT(LLM):
               prompt: str,
               history: List[List[str]] = [],
               streaming: bool = STREAMING):  # -> Tuple[str, List[List[str]]]:
-        # response=openai.Completion.create(
-        #     model="text-davinci-003",
-        #     prompt=prompt,
-        #     temperature=0,
-        #     max_tokens=MAX_TOKEN,
-        #     top_p=1.0,
-        #     frequency_penalty=0.0,
-        #     presence_penalty=0.0,
-        #     stop=["\"\"\""]
-        # ).choices[0].text
 
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                messages=[{"role": "user", "content": prompt}],
-                                                temperature=0,
+                                                messages=[{"role": ROLE, "content": prompt}],
+                                                temperature=TEMPERATURE,
                                                 max_tokens=MAX_TOKEN,
-                                                top_p=1.0,
-                                                frequency_penalty=0.0,
-                                                presence_penalty=0.0,
-                                                stop=["\"\"\""]
+                                                top_p=TOP_P,
+                                                frequency_penalty=FREQUENCECY_PENALTY,
+                                                presence_penalty=PRESENCE_PENALTY,
+                                                stop=STOP
                                                 ).choices[0].message.content
         history += [[prompt, response]]
         yield response, history
